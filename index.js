5737888d7c881d229c47468c60c8a57e59283fc1
Khumbongmayum Tonny <tonnykh@icloud.com>
2022-11-16 15:28:45 +0530

    Change object constructor to class

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const modal = document.querySelector(".modal_overlay");
-const btn = document.querySelector(".btn_add_new_book");
+const btnAddBook = document.querySelector(".btn_add_new_book");
 
 
 let deleteBtn = document.querySelectorAll(".bi-trash");
@@ -18,7 +18,7 @@ let formSubmitBtn = document.querySelector("#add_book_form");
 let myLibrary = [];
 
 
-btn.onclick = function() {
+btnAddBook.onclick = function() {
     modal.style.display = "block";
 }
 
@@ -30,72 +30,16 @@ window.onclick = function(event) {
 }
 
 
-function Book(bookName, author, totalPages, bookStatus) {
-    this.bookName = bookName;
-    this.author = author;
-    this.totalPages = totalPages;
-    this.bookStatus = bookStatus;
-}
-
-
-function addBookToLibrary() {
-    myLibrary.push(new Book(bookName, author, totalPages, bookStatus));
-}
-
-
-function Display() {
-}
-
-
-Display.prototype.add = function(book) {
-    console.log("adding to UI");
-
-    let read = (book.bookStatus === "read") ? "selected":"";
-    let reading = (book.bookStatus === "reading") ? "selected":"";
-    let wantToRead = (book.bookStatus === "wantToRead") ? "selected":"";
-    
-    let sectionGrid = document.querySelector(".section_grid");
-
-
-    let uiString =  `<section class="section_item">
-
-                        <img src="https://mlksathil.github.io/library/assets/web/svg-images/book/book3.svg" alt="Fig: Book cover image.">
-
-                        <div class="book_description">
-                        <h3>${book.bookName}</h3>
-                            <p class="author_name">by ${book.author}</p>
-                            <p>${book.totalPages} pages</p>
-                        </div>
-                            
-                        <button><i class="bi bi-trash"></i></button>
-
-                        <select>
-                            <option value="read" selected="${read}">Read</option>
-                            <option value="reading" selected="${reading}">Reading</option>
-                            <option value="wantToRead" selected="${wantToRead}">Wan to read</option>
-                        </select>
-
-                     </section>`;
-
-        sectionGrid.innerHTML += uiString;
-}
-
-Display.prototype.clear = function() {
-    formSubmitBtn.reset();
-}
-
-
 formSubmitBtn.addEventListener('submit', bookFormSubmit);
 
 
 function bookFormSubmit(e) {
 
-    let book = new Book(bookName.value, author.value, totalPages.value, bookStatus.options[bookStatus.selectedIndex].value);
-    console.log(book);
+    let bookData = new Book(bookName.value, author.value, totalPages.value, bookStatus.options[bookStatus.selectedIndex].value);
+    console.log(bookData);
 
-    let display = new Display();
-    display.add(book);
-    display.clear();
+    bookData.add(bookData);
+    bookData.clear();
 
     modal.style.display = "none";
 
@@ -103,11 +47,61 @@ function bookFormSubmit(e) {
 }
 
 
+class Book {
+
+    constructor(bookName, author, totalPages, bookStatus) {
+        this.bookName = bookName;
+        this.author = author;
+        this.totalPages = totalPages;
+        this.bookStatus = bookStatus;
+    }
+
+    add() {
+        let read = (this.bookStatus === "read") ? "selected":"";
+        let reading = (this.bookStatus === "reading") ? "selected":"";
+        let wantToRead = (this.bookStatus === "wantToRead") ? "selected":"";
+            
+        let sectionGrid = document.querySelector(".section_grid");
+        
+        let uiString =  `
+            <section class="section_item">
+        
+                <img src="https://mlksathil.github.io/library/assets/web/svg-images/book/book3.svg" alt="Fig: Book cover image.">
+            
+                <div class="book_description">
+                <h3>${this.bookName}</h3>
+                    <p class="author_name">by ${this.author}</p>
+                    <p>${this.totalPages} pages</p>
+                </div>
+                                        
+                <button><i class="bi bi-trash"></i></button>
+            
+                <select>
+                    <option value="read" selected="${read}">Read</option>
+                    <option value="reading" selected="${reading}">Reading</option>
+                    <option value="wantToRead" selected="${wantToRead}">Want to read</option>
+                </select>
+        
+            </section>
+            `;
+        
+        sectionGrid.innerHTML += uiString;
+    }
+
+    clear() {
+        formSubmitBtn.reset();
+    }
+
+}
+    
+function addBookToLibrary() {
+    myLibrary.push(new Book(bookName, author, totalPages, bookStatus));
+}
+
+
 deleteBtn.forEach(deleteCard);
 
-
 function deleteCard(item) {
-
     item.addEventListener('click', function() {
             console.log('yes press');
             item.parentElement.parentElement.remove();
